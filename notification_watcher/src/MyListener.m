8a6c4c2b9ec43ea667bf2a7f1d4b5b2b4ab740f8
Pedro Melo <melo@simplicidade.org>
2008-03-12 11:52:07 +0000

    Initial implementation of the MyListener object
    The notification name is hard-coded for now.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ notification_watcher/src/MyListener.m
@@ -0,0 +1,40 @@
+
+#import "MyListener.h"
+#include <string.h>
+
+@interface MyListener (Private)
+- (void) p_iTunesPlayStatusChanged:(NSNotification *)theNotification;
+@end
+
+@implementation MyListener
+
+- init
+{
+	if (self = [super init]) {
+		// Set up the AppleScript
+		
+		[[NSDistributedNotificationCenter defaultCenter]
+		    addObserver:self
+		       selector:@selector(p_iTunesPlayStatusChanged:)
+		    	     name:@"com.apple.iTunes.playerInfo"
+		    		 object:nil
+		];
+	}
+	return self;
+}
+
+- (void)dealloc
+{
+	[[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
+	[super dealloc];
+}
+
+- (void)p_iTunesPlayStatusChanged:(NSNotification *)theNotification
+{
+  const char *iTunesInfo = [[[theNotification userInfo] description] UTF8String];
+  
+  printf("%d\n%s\n", strlen(iTunesInfo), iTunesInfo);
+}
+
+
+@end
